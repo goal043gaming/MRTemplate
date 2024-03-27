0f0e7e71c22dd89abf077eeb687c27cc453ed977
JacquesHaan <433288@student.fontys.nl>
2024-03-27 11:10:57 +0100

    Initial commit

---
+++ Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnCube : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float speed = 2;
+    public InputActionProperty input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(input.action.WasPressedThisFrame())
+        {
+            GameObject spawnedCube = Instantiate(cubePrefab,transform.position, transform.rotation);
+            Rigidbody rb = spawnedCube.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * speed;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mixed Reality
+ Mixed Reality project for Goal043 from Jacques Haan
