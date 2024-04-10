7c0cf91d7ea8a4d3b3b3916b62a9093d691580e7
JacquesHaan <433288@student.fontys.nl>
2024-04-10 11:36:28 +0200

    Added text element for points
    Added text element as child of the goal registration. Should be tested, hardware malfunction.

--- Assets/Scripts/HoleDetection.cs
+++ Assets/Scripts/HoleDetection.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HoleDetection : MonoBehaviour
 {
 
     [SerializeField] int pointAmount;
+    [SerializeField] TMP_Text text;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == "Throwable")
         {
             Destroy(collision.gameObject);
+            text.text = pointAmount.ToString();
         }
     }
 }
