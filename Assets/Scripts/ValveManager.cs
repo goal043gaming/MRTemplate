33bf5781986e94e3ab2dd079e0ab608f9b4e1f67
jacques043 <jacques@goal043.nl>
2024-04-30 15:18:35 +0200

    Bool works with turning of the Valve
    The boolean inside the checkpoints is now updated by the valve turning and correctly adjusts the attached checkpoint state.

---
+++ Assets/Scripts/ValveManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class ValveManager : MonoBehaviour
+{
+    [SerializeField] XRKnob valveRotation;
+    [SerializeField] FlowCheckpoints connectedCheckpoint;
+    private float rotationValue;
+
+    private void Update()
+    {
+        rotationValue = valveRotation.value;
+        print(rotationValue);
+        SetState();
+    }
+
+    private void SetState()
+    {
+        if(rotationValue <= 0.9)
+        {
+            connectedCheckpoint.valveOpen = false;
+        }
+        else if(rotationValue >= 1)
+        {
+            connectedCheckpoint.valveOpen = true;
+        }
+    }
+}
