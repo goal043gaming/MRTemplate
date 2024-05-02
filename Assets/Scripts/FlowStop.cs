a2c6ea5aa5ac0fe976cd6b96f310a24fd1bd6a40
JacquesHaan <433288@student.fontys.nl>
2024-05-02 14:06:09 +0200

    Functional with Button
    The flow stop script is now functional with a physical button rather than debug code. Button still needs to be tested in VR.

--- Assets/Scripts/FlowStop.cs
+++ Assets/Scripts/FlowStop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FlowStop : MonoBehaviour
 {
@@ -10,23 +11,69 @@ public class FlowStop : MonoBehaviour
     public bool ButtonEnabled = false;
     public ParticleSystem activeEffect;
 
+    [SerializeField] float thresholdValue = 0.1f;
+    [SerializeField] float deadzone = 0.025f;
+
+    private bool isPressed;
+    private Vector3 startPos;
+    private ConfigurableJoint joint;
+
     [Header("Debug")]
     public bool PressButton = false;
-   public void ButtonPress()
+
+    private void Start()
     {
+        startPos = transform.localPosition;
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    private void Update()
+    {
+        if (PressButton)
+        {
+            ButtonPress();
+        }
+
+        if(!isPressed && GetValue() + thresholdValue >= 1)
+        {
+            ButtonPress();
+        }
+        if(isPressed && GetValue()  - thresholdValue <= 0)
+        {
+            ButtonRelease();
+        }
+
+    }
+    private void ButtonPress()
+    {
+        isPressed = true;
+
         if (ButtonEnabled)
         {
             flood.flooding = false;
             flowHandler.allowMovement = false;
             activeEffect.Stop();
         }
+
+        print("Testing1");
     }
 
-    public void Update()
+    private void ButtonRelease()
     {
-        if(PressButton)
+        isPressed = false;
+
+        print("Testing2");
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(startPos, transform.localPosition) / joint.linearLimit.limit;
+
+        if(Mathf.Abs(value) < deadzone)
         {
-            ButtonPress();
+            value = 0;
         }
+
+        return Mathf.Clamp(value, -1f, 1f);
     }
 }
